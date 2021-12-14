f84846e85921b850806bc9a997e30e6eb5640b3c
NicoJ-Git <85492204+NicoJ-Git@users.noreply.github.com>
2021-12-14 12:39:34 +0100

    with repository

---
+++ app/src/main/java/com/example/pockedex/repository/PokemonRepository.java
@@ -0,0 +1,43 @@
+package com.example.pockedex.repository;
+
+import com.example.pockedex.R;
+import com.example.pockedex.model.Pokemon;
+import com.example.pockedex.model.PokemonType;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PokemonRepository {
+
+    private final List<Pokemon> pokemons;
+
+    public PokemonRepository() {
+
+
+        //(nino1)creation de la liste de pokemons
+
+        pokemons = new ArrayList<Pokemon>();
+        pokemons.add(new Pokemon(1, R.drawable.ic_baseline_terrain_24, "Bulbizarre", "Bulbasaure", PokemonType.GRASS));
+        pokemons.add(new Pokemon(2, R.drawable.ic_baseline_local_fire_department_24, "Charizard", "Charizard", PokemonType.FIRE));
+        pokemons.add(new Pokemon(3, R.drawable.ic_baseline_waves_24, "Blastoise", "Blastoise", PokemonType.WATER));
+        pokemons.add(new Pokemon(4, R.drawable.ic_baseline_terrain_24, "Bulbizarre", "Bulbasaure", PokemonType.GRASS));
+        pokemons.add(new Pokemon(5, R.drawable.ic_baseline_local_fire_department_24, "Charizard", "Charizard", PokemonType.FIRE));
+        pokemons.add(new Pokemon(6, R.drawable.ic_baseline_waves_24, "Blastoise", "Blastoise", PokemonType.WATER));
+        pokemons.add(new Pokemon(7, R.drawable.ic_baseline_terrain_24, "Bulbizarre", "Bulbasaure", PokemonType.GRASS));
+        pokemons.add(new Pokemon(8, R.drawable.ic_baseline_local_fire_department_24, "Charizard", "Charizard", PokemonType.FIRE));
+        pokemons.add(new Pokemon(9, R.drawable.ic_baseline_waves_24, "Blastoise", "Blastoise", PokemonType.WATER));
+        pokemons.add(new Pokemon(10, R.drawable.ic_baseline_terrain_24, "Bulbizarre", "Bulbasaure", PokemonType.GRASS));
+        pokemons.add(new Pokemon(11, R.drawable.ic_baseline_local_fire_department_24, "Charizard", "Charizard", PokemonType.FIRE));
+        pokemons.add(new Pokemon(12, R.drawable.ic_baseline_waves_24, "Blastoise", "Blastoise", PokemonType.WATER));
+        pokemons.add(new Pokemon(13, R.drawable.ic_baseline_terrain_24, "Bulbizarre", "Bulbasaure", PokemonType.GRASS));
+        pokemons.add(new Pokemon(14, R.drawable.ic_baseline_local_fire_department_24, "Charizard", "Charizard", PokemonType.FIRE));
+        pokemons.add(new Pokemon(15, R.drawable.ic_baseline_waves_24, "Blastoise", "Blastoise", PokemonType.WATER));
+
+
+    }
+
+    public List<Pokemon> getPokemons(){
+        return pokemons;
+    }
+
+}
