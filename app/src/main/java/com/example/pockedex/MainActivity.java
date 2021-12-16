509699d6e9b16f4a1ce8cce0f1c6e2c430349e35
NicoJ-Git <85492204+NicoJ-Git@users.noreply.github.com>
2021-12-16 10:22:25 +0100

    with repository

--- app/src/main/java/com/example/pockedex/MainActivity.java
+++ app/src/main/java/com/example/pockedex/MainActivity.java
@@ -36,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setAdapter(adapter);
 
         
-        adapter.getItemId(mPokemonRepository.getPokemons());
+        adapter.getItemId(mPokemonRepository).get(PokemonRepository.class);
     }
 
 }
